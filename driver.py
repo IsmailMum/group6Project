f88519b511565893bc1ee9159c46777865f864ca
İsmail Mum <imum@gtu.edu.tr>
2021-06-27 16:33:56 +0300

    adding files

---
+++ driver.py
@@ -0,0 +1,63 @@
+import bluetooth
+import sys
+import re
+import pyautogui
+re.compile('<title>(.*)</title>')
+
+target_name = "HC-05"
+target_address = "00:20:10:09:10:F6"
+
+nearby_devices = bluetooth.discover_devices()
+
+for bdaddr in nearby_devices:
+    if target_name == bluetooth.lookup_name( bdaddr ):
+        target_address = bdaddr
+        break
+
+if target_address is not None:
+    print("found target bluetooth device with address "), target_address
+else:
+    print("could not find target bluetooth device nearby")
+    
+
+bd_addr = "00:20:10:09:10:F6"
+
+port = 1
+sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect((bd_addr, port))
+print ('Connected')
+current_key = 'x'
+while(True):
+    input_data = sock.recv(12)
+    decoded_input_data = input_data.decode()
+    x = list (map (int, re.findall(r'\d+', decoded_input_data)))
+    if (len (x) == 2):
+        
+        if x[0] <= 500 and x[1] <= 500 and current_key != 'w':
+            current_key = 'w'
+            pyautogui.keyDown(current_key)
+            	
+        if x[0] >= 3500 and x[1] >= 3500 and current_key != 's':
+            current_key = 's'
+            pyautogui.keyDown(current_key)
+
+        if x[0] <= 500 and x[1] >= 3500 and current_key != 'a':
+            current_key = 'a'
+            pyautogui.keyDown(current_key)
+            
+        if x[0] >= 3500 and x[1] <= 500 and current_key != 'd':
+            current_key = 'd'
+            pyautogui.keyDown(current_key)
+            		
+        if x[0] >= 2000 and x[0] <= 3000 and x[1] >= 2000 and x[1] <= 3000:
+        	pyautogui.keyUp(current_key)
+
+        if x[0] >= 2000  and x[0] <= 3000 and x[1] >= 2000 and x[1] <= 3000:
+            current_key = 'x'
+            
+
+
+	
+
+
+sock.close()
